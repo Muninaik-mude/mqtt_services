703c56ab0e4bf03c7a1c833410f5360d2a7166ee
Muninaik-mude <munikumar.m@orotron.com>
2025-02-05 15:42:50 +0530

    Update mqtt_app_state.dart

--- lib/mqtt/state/mqtt_app_state.dart
+++ lib/mqtt/state/mqtt_app_state.dart
@@ -1,32 +1,3 @@
-// import 'package:flutter/cupertino.dart';
-
-// enum MQTTAppConnectionState { connected, disconnected, connecting }
-// class MQTTAppState with ChangeNotifier{
-//   MQTTAppConnectionState _appConnectionState = MQTTAppConnectionState.disconnected;
-//   String _receivedText = '';
-//   String _historyText = '';
-
-//   void setReceivedText(String text) {
-//     _receivedText = text;
-//     _historyText = _historyText + '\n' + _receivedText;
-//     notifyListeners();
-//   }
-//   void setAppConnectionState(MQTTAppConnectionState state) {
-//     _appConnectionState = state;
-//     notifyListeners();
-//   }
-
-//   String get getReceivedText => _receivedText;
-//   String get getHistoryText => _historyText;
-//   MQTTAppConnectionState get getAppConnectionState => _appConnectionState;
-
-//   void setConnectionState(MQTTAppConnectionState disconnected) {}
-
-//   void addReceivedMessage(String payload) {}
-
-// }
-//another code
-
 import 'package:flutter/cupertino.dart';
 
 enum MQTTAppConnectionState { connected, disconnected, connecting }
